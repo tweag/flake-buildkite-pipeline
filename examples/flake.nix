0957c696e8daf6b588176dde5984d22e4069c673
Bryan Honof <bryan.honof@tweag.io>
2022-08-23 14:30:24 +0200

    docs: Add an example flake
    Signed-off-by: Bryan Honof <bryan.honof@tweag.io>

---
+++ examples/flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "flake-buildkite-pipeline example";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    flake-buildkite-pipeline.url = "github:tweag/flake-buildkite-pipeline";
+  };
+
+  outputs = { self, nixpkgs, flake-buildkite-pipeline }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.hello = pkgs.hello;
+
+      checks.${system} = let
+        mkHelloCheck = { checkName, traditional ? false, greeting ? "" }:
+          let
+            inherit (pkgs.lib.strings) escapeShellArg;
+            inherit (self.packages.${system}) hello;
+            want = if traditional then
+              "hello, world"
+            else if greeting != "" then
+              "${greeting}"
+            else
+              "Hello, world!";
+            shellArgs = if traditional then
+              "--traditional"
+            else if greeting != "" then
+              "--greeting=${escapeShellArg greeting}"
+            else
+              "";
+            command = "${hello}/bin/hello ${shellArgs}";
+          in (pkgs.runCommandNoCC "${checkName}" {
+            meta.checkDescription = "${checkName}";
+          } ''
+            set -o verbose
+            want='${want}'
+            got="$(${command})"
+            [ "$want" == "$got" ]
+            touch $out
+          '');
+      in {
+        hello = mkHelloCheck { checkName = "hello"; };
+
+        helloTraditional = mkHelloCheck {
+          checkName = "helloTraditional";
+          traditional = true;
+        };
+
+        helloBryan = mkHelloCheck {
+          checkName = "helloBryan";
+          greeting = "Hello, Bryan!";
+        };
+      };
+
+      pipelines.buildkite.steps = flake-buildkite-pipeline.lib.flakeSteps {
+        commonExtraStepConfig = {
+          agents = [ "nix" ];
+          plugins = [{ "thedyrt/skip-checkout#v0.1.1" = null; }];
+        };
+      } self;
+    };
+}
